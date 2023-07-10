bd815bf2dbe6ff4e826a6034e2d4256f37cbef4c
Micaela Ceballos <83962558+MicaCblls@users.noreply.github.com>
2023-07-10 09:53:00 -0300

    Update Placeholder.jsx

--- src/Placeholder.jsx
+++ src/Placeholder.jsx
@@ -4,18 +4,20 @@ import ProductCard from './ProductCard'; // Asegúrate de importar el componente
 
 // eslint-disable-next-line react/prop-types
 const PlaceholderProducts = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [products, setProducts] = useState([])
-  const url = import.meta.env.VITE_REACT_APP_API
-
-  useEffect(() => {
-    fetch(url + '/productos')
-      .then(response => response.json())
-      .then(data => setProducts(data)).catch(error => console.log(error))
-  },[url])
-
+ const products = [
+    { id: 1, description: 'Producto 1', price: 9.99 },
+    { id: 2, description: 'Producto 2', price: 19.99 },
+    { id: 3, description: 'Producto 3', price: 14.99 },
+    { id: 4, description: 'Producto 4', price: 24.99 },
+    { id: 5, description: 'Producto 5', price: 12.99 },
+    { id: 6, description: 'Producto 6', price: 29.99 },
+    { id: 7, description: 'Producto 7', price: 17.99 },
+    { id: 8, description: 'Producto 8', price: 21.99 },
+    { id: 9, description: 'Producto 9', price: 16.99 },
+    { id: 10, description: 'Producto 10', price: 11.99 }
+  ];
   return (
-    <ProductCard products={products?.slice(0,50)} />
+    <ProductCard products={products} />
   );
 };
 
