c07c6eb705af739b6749985e8a6f38e0778c7141
MicaCblls <micaelaceballos411@gmail.com>
2023-07-10 16:07:21 -0300

    fix card

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
 
-
 // eslint-disable-next-line react/prop-types
-const ProductCard = ({products}) => {
+const ProductCard = ({ products }) => {
   // eslint-disable-next-line no-unused-vars
 
   // eslint-disable-next-line react/prop-types
@@ -12,11 +11,10 @@ const ProductCard = ({products}) => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-
       {products?.map((product, index) => (
         <div className="bg-white p-4 shadow-md text-header" key={index}>
-          <h3 className="text-lg font-semibold mb-2">{product.descripcion}</h3>
-          <p className="text-gray-600">${product.precio}</p>
+          <h3 className="text-lg font-semibold mb-2">{product.description}</h3>
+          <p className="text-gray-600">${product.price}</p>
         </div>
       ))}
     </div>
