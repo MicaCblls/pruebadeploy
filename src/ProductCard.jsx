b2b5109ac5737725caf7bae7886540bdeb70670e
Micaela Ceballos <83962558+MicaCblls@users.noreply.github.com>
2023-07-09 23:21:11 -0300

    Update ProductCard.jsx

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -14,9 +14,9 @@ const ProductCard = ({products}) => {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
 
       {products?.map((product, index) => (
-        <div className="bg-white p-4 shadow-md" key={index}>
-          <h3 className="text-lg font-semibold mb-2">{product.description}</h3>
-          <p className="text-gray-600">${product.price}</p>
+        <div className="bg-white p-4 shadow-md text-header" key={index}>
+          <h3 className="text-lg font-semibold mb-2">{product.descripcion}</h3>
+          <p className="text-gray-600">${product.precio}</p>
         </div>
       ))}
     </div>
